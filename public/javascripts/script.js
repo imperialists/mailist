a1dfb4a27c472bb218cdcb057f7f8d35baeb6e31
Petr Hosek <petrhosek@gmail.com>
2012-02-26 18:04:23 +0000

    fix web frontend

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,24 +1,19 @@
 jQuery(function($){
-  $('.bar').mosaic({
-	  animation	:	'slide'		//fade or slide
-  });
-
-  $('#groups').height(Math.max($('#threads').height(),$('#groups').height()));
   displayPinnedThreads();
   displaySubscriptions();
 });
 
 function displaySubscriptions() {
-	 var divs = [];
-     $.getJSON('/subscribed', function(data) {
+  var divs = [];
+    $.getJSON('/subscribed', function(data) {
        $.each(data, function(key, label) {
-        // REPLACE HASH WITH THE ADRESS OF A BROWSING PAGE
-       var div = "<a class=\"menu\" href=\"label.html?label=" + label + "\">" + label + "</a><br />";
-       divs.push(div);
-     });
-     $('#groups').prepend(divs.join());
-     $('#groups').prepend("<h4 style=\"margin-top: 3px\">Groups</h4>");
-  });
+          // REPLACE HASH WITH THE ADRESS OF A BROWSING PAGE
+         var div = "<a class=\"menu\" href=\"/label/" + label + "\">" + label + "</a><br />";
+         divs.push(div);
+       });
+       $('#groups').prepend(divs.join());
+       $('#groups').prepend("<h4 style=\"margin-top: 3px\">Groups</h4>");
+    });
 }
 
 function displayPinnedThreads() {
@@ -27,7 +22,8 @@ function displayPinnedThreads() {
   var tid = 0;
   $.getJSON('/pinned', function(data) {
     $.each(data, function(key, thread) {
-
+	  var m = thread.messages[0];
+		  
       var comment = "<div class=\"highslide-html-content\" id=\"thread-" + tid + "\">";
       comment += "<div class=\"highslide-header\">";
       comment += "<ul>";
@@ -39,22 +35,19 @@ function displayPinnedThreads() {
       comment += "<div class=\"highslide-body\">";
 
       comment += "<div class=\"row\">";
-      comment += "<div class=\"picture\"><img src=\"images/big.jpg\" alt=\"\" width=\"200\" height=\"160\" /></div>";
       comment += "<div class=\"wrapper\">";
-      comment += "<h1>Haskell lecture</h1>";
+      comment += "<h1>" + m.subject + "</h1>";
       comment += "<p class=\"separator\">&nbsp;</p>";
       comment += "<input type=\"text\" placeholder=\"Subject\" />";
       comment += "<textarea placeholder=\"Type your message. Slowly.\"></textarea>";
-      comment += "<p class=\"submit\">Characters: 24, Words: 4 <input type=\"submit\" value=\"submit\" /></p>";
       comment += "</div>";
       comment += "<div class=\"clearfix\"></div>";
       comment += "</div>";
           
-      $.each(thread.messages, function(key3, message) {
+      $.each(thread.messages.slice(1), function(key3, message) {
         comment += "<div class=\"row-small\">";
-        comment += "<div class=\"picture-small\"><img src=\"images/big.jpg\" alt=\"\" width=\"50\" height=\"40\" /></div>";
         comment += "<div class=\"wrapper-small\">";
-        comment += "<h4>No Haskell lecture on Friday, 09 February</h4>";
+        comment += "<h4>" + message.subject + "</h4>";
         comment += "<p>" + message.body + "</p>";
         comment += "</div>";
         comment += "<div class=\"clearfix\"></div>";
@@ -62,14 +55,21 @@ function displayPinnedThreads() {
       });
 
       comment += "</div></div>";
-      comment_list.push(comment);
-          
+      comment_list.push(comment);          
 
-      var div = "<div class=\"mosaic-block bar\"><a href=\"#\" class=\"highslide mosaic-overlay\" onclick=\"return hs.htmlExpand(this, { contentId: 'thread-" + tid + "' } )\"><div class=\"details\"><h4>Haskell Lecture</h4><p>Messages: #3<br />Pins: #10<br />Likes: #7<br />Last discussed: 13/03/2012</p><input type=\"button\" value=\"Pin\" /><input type=\"button\" value=\"Like\" /><input type=\"button\" value=\"Comment\" /></div></a><div class=\"mosaic-backdrop\"><img src=\"http://buildinternet.s3.amazonaws.com/projects/mosaic/mightyicons.jpg\" /></div></div>";
+      var div = "<div class=\"mosaic-block bar\"><a href=\"#\" class=\"highslide mosaic-overlay\" onclick=\"return hs.htmlExpand(this, { contentId: 'thread-" + tid + "' } )\">" +
+	  	"<div class=\"details\"><h4>" + m.subject + "</h4><p>Messages: #" + thread.messages.length + "<br />Pins: #10<br />Likes: #7<br />Last discussed: 13/03/2012</p>" +
+		"<input type=\"button\" value=\"Pin\" /><input type=\"button\" value=\"Like\" /><input type=\"button\" value=\"Comment\" /></div>" +
+		"</a><div class=\"mosaic-backdrop\"></div></div>";
       thread_list.push(div);
       tid++;
     });
-    $('#t').html(thread_list.join());
-    $('#c').html(comment_list.join());
+    $('#t').html(thread_list.join(''));
+    $('#c').html(comment_list.join(''));
+	
+    $('.bar').mosaic({
+  	  animation	:	'slide'		//fade or slide
+    });
+    $('#groups').height(Math.max($('#threads').height(),$('#groups').height()));
   });
 }
